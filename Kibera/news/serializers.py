f47b4a5579e8070e4d363c899d8014824bfb569b
khusan9889 <k.xusan_2003@mail.ru>
2023-01-24 22:18:59 +0500

    CRUD is done

--- Kibera/news/serializers.py
+++ Kibera/news/serializers.py
@@ -2,23 +2,29 @@ from rest_framework import serializers
 from .models import News
 
 
-class NewsSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=255)
-    body = serializers.CharField()
-    author = serializers.CharField()
-    created_date = serializers.DateTimeField(read_only=True)
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = '__all__'
+
     
-    def create(self, validated_data):
-        return News.objects.create(**validated_data)
+#Более подробное описание Сериализаторов
+    # title = serializers.CharField(max_length=255)
+    # body = serializers.CharField()
+    # author = serializers.CharField()
+    # created_date = serializers.DateTimeField(read_only=True)
+    
+    # def create(self, validated_data):
+    #     return News.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance.title)
-        instance.body = validated_data.get("body", instance.body)
-        instance.author = validated_data.get("author", instance.author)
-        instance.created_date = validated_data.get("created_date", instance.created_date)
-        instance.save()
-        return instance
+    # def update(self, instance, validated_data):
+    #     instance.title = validated_data.get("title", instance.title)
+    #     instance.body = validated_data.get("body", instance.body)
+    #     instance.author = validated_data.get("author", instance.author)
+    #     instance.created_date = validated_data.get("created_date", instance.created_date)
+    #     instance.save()
+    #     return instance
 
-    def delete(self, instance):
-        instance.delete()
-        return instance
+    # def delete(self, instance):
+    #     instance.delete() 
+    #     return instance
